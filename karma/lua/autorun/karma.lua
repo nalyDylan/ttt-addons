2ee83ba548c692167685a9178ebbf3b7d484f95b
Dylan Wilson <20782994+nalyDylan@users.noreply.github.com>
2020-09-06 00:15:33 -0400

    Helps if I include the file I changed

--- karma/lua/autorun/karma.lua
+++ karma/lua/autorun/karma.lua
@@ -1,14 +1,9 @@
 local function KT()
-	local count = 0
-	for _,p in pairs(player.GetAll()) do
-		if p:IsTerror() then
-			count = count+1
-		end
-	end
+	local count = #player.GetAll() - #player.GetBots() - #team.GetPlayers(TEAM_SPECTATOR)
 	if count < 4 then
 		RunConsoleCommand("ttt_karma", "0")
     		return
 	end
-  RunConsoleCommand("ttt_karma", "1")
+	RunConsoleCommand("ttt_karma", "1")
 end
 hook.Add("TTTBeginRound", "KarmaToggle", KT)
